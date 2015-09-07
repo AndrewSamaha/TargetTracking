d66c2858645a6d691293fd013e609dd3317c482c
Andrew Samaha <andrew.samaha@gmail.com>
2015-09-07 08:04:03 -0400

    dfd

--- main.js
+++ main.js
@@ -52,9 +52,25 @@ function init() {
     
 }
 
+function toggleFullScreen() {
+  var doc = window.document;
+  var docEl = doc.documentElement;
+
+  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+  if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+    requestFullScreen.call(docEl);
+  }
+  else {
+    cancelFullScreen.call(doc);
+  }
+}
+
 function startgame() {
     document.getElementById("startbutton").style.visibility = "hidden";
     document.body.requestFullscreen();
+    toggleFullScreen();
 }
 
 function currenttime() {
