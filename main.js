0c729299fb2443e2713522d183c37821216f8fea
Andrew Samaha <andrew.samaha@gmail.com>
2015-09-06 22:05:29 -0400

    yea yea

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
+function accelerometerUpdate(e) {
+   var aX = event.accelerationIncludingGravity.x*1;
+   var aY = event.accelerationIncludingGravity.y*1;
+   var aZ = event.accelerationIncludingGravity.z*1;
+   //The following two lines are just to calculate a
+   // tilt. Not really needed. 
+   xPosition = Math.atan2(aY, aZ);
+   yPosition = Math.atan2(aX, aZ);
+}
 
 function init() {
+    if (window.DeviceMotionEvent == undefined) {
+        //No accelerometer is present. Use buttons. 
+        alert("no accelerometer");
+    }
+    else {
+        alert("accelerometer found");
+        window.addEventListener("devicemotion", accelerometerUpdate, true);
+    }
     
     tic();
     
