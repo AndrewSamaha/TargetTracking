66db170e92ac163083cd7fd38e2a9f9f498e1937
Andrew Samaha <andrew.samaha@gmail.com>
2015-09-08 19:21:52 -0400

    dfdf

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ function init() {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
     renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
     
     gamestarttime = -1;
     
