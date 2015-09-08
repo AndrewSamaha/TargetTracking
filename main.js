71e570a69deda04122adab7e79e2334811511060
Andrew Samaha <andrew.samaha@gmail.com>
2015-09-08 19:01:04 -0400

    dfd

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var orientationmsg = "notdefined";
 var scene;
 var camera;
 var remderer;
+var cube;
 var gamestarttime;
 
 function accelerometerUpdate(e) {
@@ -76,7 +77,7 @@ function startgame() {
     document.body.appendChild( renderer.domElement );
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
     var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
+    cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
 
     camera.position.z = 5;
@@ -90,6 +91,8 @@ function currenttime() {
 
 function tic() {
     requestAnimationFrame(tic);
+    cube.rotation.x += 0.1;
+    cube.rotation.y += 0.1;
     renderer.render( scene, camera );
     /*if (currenttime() > 3000 && shown == false) {
         shown = true;
