a14981a7b0fe2b091c6135488964d5ef555611dd
Andrew Samaha <andrew.samaha@gmail.com>
2015-09-09 09:51:19 -0400

    dfdf

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ function accelerometerUpdate(e) {
         xInitialPositions.push(xPosition);
         yInitialPositions.push(yPosition);
     } else {
-        var sig = 50;
+        var sig = 10;
         var scaler = 10;
         if (xInitialPosition == 1010) {
             var xsum = 0, xavg;
