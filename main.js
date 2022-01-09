71f7499ad830906a338e025642276e54531568f0
Andrew Samaha <andrew.samaha@gmail.com>
2022-01-08 21:39:35 -0500

    added vite and aesthetic changes

--- main.js
+++ main.js
@@ -1,218 +1,123 @@
-var componentA;
-var componentB;
-var multipleschedule;
-var changeover;
+
+
+var DataCollector = function() {
+    this.debug = 0;
+    if (this.debug) {
+        this.$element = $("<div id=datacollector></div>");
+        this.$element.css("position","fixed");
+        this.$element.css("left","100px");
+        this.$element.css("width","80%");
+        this.$element.css("height", "100px");
+        this.$element.css("bottom","100px");
+        this.$element.css("overflow-y","scroll");
+        this.$element.css("background","rgba(200,200,200,.5)");
+        this.$element.css("padding","2px 2px 2px 2px");
+        this.$element.css("border-style","solid");
+        this.$element.css("border-width","1px");
+        this.$element.css("font-size",".75em");
+        $("body").append( this.$element );
+    }
+    /*
+    position: fixed;
+                right: 200px;
+                width: 300px;
+
+                background-color: gray;
+    */
+    this.data = [];
+    this.startTime = Date.now();
+    
+    this.event = function(datum) {
+        if (!datum) datum = {undefined: true};
+        if (!datum.hasOwnProperty("event")) datum.event = "undefined";
+        if (!datum.hasOwnProperty("time")) datum.time = Date.now();
+        this.data.push(datum);
+        if (this.debug)
+            this.$element.prepend((Date.now() - this.startTime) + "\t" + JSON.stringify(datum) + "<br>");
+    }
+    this.click = function(datum) {
+        if (!datum.hasOwnProperty("event")) datum.event = "click";
+        this.event(datum);
+        console.log(datum);
+    }
+}
+
+var dataCollector;
+var shrapnelManager;
+var targetNum;
+var target;
+var distanceToTarget;
+var lastTicTime;
+var delta_t;
+var mX;
+var mY;
+var lastError;
+var debug = 1;
+var audioReady = 0;
+
+function calculateDistance(t, mouseX, mouseY) {
+
+    return Math.floor(Math.sqrt(Math.pow(mouseX - (t.last_x+t.radius), 2) + Math.pow(mouseY - (t.last_y+t.radius), 2)));
+}
+
+
+
+const newTarget = (t) => {
+    targetNum++;
+    target = {};
+    target = new Target({dataCollector: dataCollector});
+    target.shrapnelManager = shrapnelManager;
+    if (!t) t=0;
+    target.startIn(t);
+    if (audioReady == 0) {
+        target.explosion_sound.load();
+        audioReady = 1;
+    }
+    
+    return target;
+}
 
 function init() {
-  $('body').css({position: "fixed"});
-
-  /*
-  MULTA_button = new Button();
-  MULTA_button.create();
-  MULTA_button.setPosition(15, 15, $(window).width()-15, $(window).height() - 15);
-
-  MULTB_button = new Button();
-  MULTB_button.create();
-  MULTB_button.setPosition(15, 15, $(window).width()-15, $(window).height() - 15);
-
-  MULTA = new Schedule();
-  MULTA.configure({
-    interval_ms: 5000,
-    responseRequirement: true,
-    label: "MULTA",
-    stateColors: [
-      {state: "running", color: "blue"},
-      {state: "paused",  color: "grey"},
-      {state: "timedout", color: "green"}
-    ],
-    button: MULTA_button
-  });
-
-
-  MULTB = new Schedule();
-  MULTB.configure({
-    interval_ms: 5000,
-    responseRequirement: true,
-    label: "MULTB",
-    stateColors: [
-      {state: "running", color: "blue"},
-      {state: "paused",  color: "grey"},
-      {state: "timedout", color: "green"}
-    ],
-    button: MULTB_button
-  });
-*/
-  changeover = (function() {
-    var button = new Button();
-    button.create();
-    button.setPosition($(window).width()/3, $(window).height()*.5, $(window).width()*.4, 100);
-    var d = new VIDistribution(30000, 12);
-
-    var VT30 = new Schedule();
-    VT30.configure({
-      interval_ms: 10000, // this value is ignored because of the following line
-      distribution: d.intervals,
-      responseRequirement: false,
-      label: "VT30sChangeover",
-      stateColors: [
-        {state: "running", color: "#e6ccb3"},
-        {state: "paused",  color: "grey"},
-        {state: "timedout", color: "green"}
-      ],
-      button: button,
-      waitForUserAfterTimeout: true,
-      timeout_callback: function(s) { multipleschedule.toggleComponent(); s.start(); }
-    });
-    return VT30;
-  })();
-
-  componentA = new ConcurrentSchedule();
-  componentA.setLabel("componentA");
-
-  componentB = new ConcurrentSchedule();
-  componentB.setLabel("componentA");
-
-  componentA.addSchedule( (function() {
-    var concurrent = new ConcurrentSchedule();
-    concurrent.setLabel("concurrentscheduleA");
-    concurrent.addSchedule( (function() {
-      console.log("inside concurrent.addSchedule");
-      var button = new Button();
-      button.create();
-      button.setPosition(15, 15, $(window).width()*.4, 100);
-
-      var FR1 = new Schedule();
-      FR1.configure({
-        interval_ms: 0,
-        responseRequirement: true,
-        label: "BEH A IN COMPONENT A",
-        stateColors: [
-          {state: "running", color: "#ddff99"},
-          {state: "paused",  color: "grey"},
-          {state: "timedout", color: "green"}
-        ],
-        button: button,
-        waitForUserAfterTimeout: false,
-        //pretimeout_callback: function(s) { changeover.pause(); },
-        timeout_callback: function(s) { /*changeover.unpause();*/ s.start(); }
-      });
-      return FR1;
-    })() );
-    /*
-    concurrent.addSchedule( (function() {
-      var button = new Button();
-      button.create();
-      button.setPosition($(window).width()/2, 15, $(window).width()*.4, 100);
-
-      var ext = new Schedule();
-      ext.configure({
-        interval_ms: 10000,
-        responseRequirement: true,
-        extinction: true,
-        label: "beh b in component a",
-        stateColors: [
-          {state: "running", color: "#cccccc"},
-          {state: "paused",  color: "grey"},
-          {state: "timedout", color: "green"}
-        ],
-        button: button,
-        waitForUserAfterTimeout: false,
-        //pretimeout_callback: function(s) { changeover.pause(); },
-        timeout_callback: function(s) { //changeover.unpause();
-        s.start(); }
-      });
-      return ext;
-    })() );
-    */
-    return concurrent;
-  })() );
-
-  componentB.addSchedule( (function() {
-    var concurrent = new ConcurrentSchedule();
-    concurrent.setLabel("concurrentscheduleB");
-    /*
-    concurrent.addSchedule( (function() {
-      var button = new Button();
-      button.create();
-      button.setPosition(15, 15, $(window).width()*.4, 100);
-
-      var ext = new Schedule();
-      ext.configure({
-        interval_ms: 10000,
-        responseRequirement: true,
-        extinction: true,
-        label: "beh a in component b",
-        stateColors: [
-          {state: "running", color: "#cccccc"},
-          {state: "paused",  color: "grey"},
-          {state: "timedout", color: "green"}
-        ],
-        button: button,
-        waitForUserAfterTimeout: false,
-        //pretimeout_callback: function(s) { changeover.pause(); },
-        timeout_callback: function(s) {
-        //changeover.unpause();
-         s.start(); }
-      });
-      return ext;
-    })() );
-    */
-    concurrent.addSchedule( (function() {
-      var button = new Button();
-      button.create();
-      button.setPosition($(window).width()/2, 15, $(window).width()*.4, 100);
-
-      var FR1 = new Schedule();
-      FR1.configure({
-        interval_ms: 0,
-        responseRequirement: true,
-        label: "BEH B IN COMPONENT B",
-        stateColors: [
-          {state: "running", color: "#b3e6ff"},
-          {state: "paused",  color: "grey"},
-          {state: "timedout", color: "green"}
-        ],
-        button: button,
-        waitForUserAfterTimeout: false,
-        //pretimeout_callback: function(s) { changeover.pause(); },
-        timeout_callback: function(s) { //changeover.unpause();
-           s.start(); }
-      });
-      return FR1;
-    })() );
-    return concurrent;
-  })() );
-
-  multipleschedule = new MultipleSchedule();
-  multipleschedule.setLabel("multipleschedule");
-  multipleschedule.addComponent( componentA );
-  multipleschedule.addComponent( componentB );
-  multipleschedule.addChangeOverSchedule( changeover );
-  multipleschedule.start();
-
-
-
-  console.log("init'd");
-  window.requestAnimationFrame( tic );
-
-
+    targetNum = 0;
+    dataCollector = new DataCollector();
+    shrapnelManager = new ShrapnelManager();
+    shrapnelManager.dataCollector = dataCollector;
+    
+    newTarget();
+    
+    $(document).mousemove((function(e) {
+        mX = e.pageX;
+        mY = e.pageY;
+        distanceToTarget = calculateDistance(target, mX, mY);
+    })).bind(this);
+    tic();
 }
 
 function tic() {
-  //console.log("tic " + Date.now());
-  if (!multipleschedule.isRunning()) {
-    console.log("tic! multipleschedule is not running yet");
-    return;
-  }
-  //if (FT.getState() == "configured") FT.start();
-
-  multipleschedule.tic( Date.now() );
-  //FT.tic();
-  window.requestAnimationFrame( tic );
+    requestAnimationFrame(tic);
+    target.tic();
+    
+    //debug
+    if (debug && Date.now() % 5 == 0) {
+        var dS = "";
+        /*dS += "distanceToTarget : " + distanceToTarget;
+        dS += "<br>liveCalculateDist: " + calculateDistance(target, mX, mY);
+        dS += "<br>x: " + target.last_x;
+        dS += "<br>y: " + target.last_y;
+        dS += "<br>mouseX: " + mX;
+        dS += "<br>mouseY: " + mY;
+        dS += "<br>targetX: " + Math.floor(target.$element.offset().left);
+        dS += "<br>targetY: " + Math.floor(target.$element.offset().top);
+        dS += "<br>lastError: " + lastError;
+        */
+        // dS += "fps: " + (Math.floor(1000/delta_t));
+        // $("#debug").html(dS);
+    }
+    shrapnelManager.tic();
+    delta_t = Date.now() - lastTicTime;
+    
+    if (!target.alive) newTarget(1500);
+    lastTicTime = Date.now();
 }
 
-$( document ).ready(function() {
-  console.log("calling init");
-  init();
-
-});
+window.onload=init;
